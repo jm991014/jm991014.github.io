0f932e5299b38620ca590f7837b7a74cf95e3b12
Jong <jm991014@gmail.com>
2024-01-27 22:25:33 +0900

    Feat: Progress bar

--- public/js/script.js
+++ public/js/script.js
@@ -14,14 +14,16 @@
   }, false);
 })(document);
 
-// var element = document.documentElement,
-//     body = document.body,
-//     scrollTop = 'scrollTop',
-//     scrollHeight = 'scrollHeight',
-//     progress = document.querySelector('.progress-bar'),
-//     scroll;
+document.addEventListener('DOMContentLoaded', function() {
+  var element = document.documentElement,
+    body = document.body,
+    scrollTop = 'scrollTop',
+    scrollHeight = 'scrollHeight',
+    progress = document.querySelector('.progress-bar'),
+    scroll;
 
-// document.addEventListener('scroll', function() {
-//     scroll = (element[scrollTop] || body[scrollTop]) / ((element[scrollHeight] || body[scrollHeight]) - element.clientHeight) * 100;
-//     progress.style.setProperty('--scroll', scroll + '%');
-// });
+  document.addEventListener('scroll', function() {
+    scroll = (element[scrollTop] || body[scrollTop]) / ((element[scrollHeight] || body[scrollHeight]) - element.clientHeight) * 100;
+    progress.style.setProperty('--scroll', scroll + '%');
+  });
+});
